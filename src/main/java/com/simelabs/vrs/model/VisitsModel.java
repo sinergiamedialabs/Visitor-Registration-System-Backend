fadd1f539bfddc27aff215ad225fd237deb85af2
Anjana James <anjana.james@simelabs.com>
2024-08-13 17:54:07 +0530

    setup

---
+++ src/main/java/com/simelabs/vrs/model/VisitsModel.java
@@ -0,0 +1,7 @@
+package com.simelabs.vrs.model;
+
+import lombok.Data;
+
+@Data
+public class VisitsModel {
+}
---
+++ src/main/java/com/simelabs/vrs/request/VisitsRequest.java
@@ -0,0 +1,7 @@
+package com.simelabs.vrs.request;
+
+import lombok.Data;
+
+@Data
+public class VisitsRequest {
+}
