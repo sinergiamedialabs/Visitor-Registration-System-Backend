7247c184725493529b20f9b9eafa494b7a03c2e7
BipinRam25 <bipin.ram@simelabs.com>
2024-08-13 16:14:56 +0530

    project structure

---
+++ src/main/java/com/simelabs/vrs/VisitorRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.simelabs.vrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/simelabs/vrs/entity/EventEntity.java
@@ -0,0 +1,13 @@
+package com.simelabs.vrs.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name = "")
+public class EventEntity {
+}
