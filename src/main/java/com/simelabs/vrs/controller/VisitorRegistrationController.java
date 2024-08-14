0ff9236b14c50b90c4a7406c88316d461a95925c
megha-mohandas <megha.mohandas@simelabs.com>
2024-08-14 12:20:25 +0530

    cors orgin error fix

--- src/main/java/com/simelabs/vrs/controller/VisitorRegistrationController.java
+++ src/main/java/com/simelabs/vrs/controller/VisitorRegistrationController.java
@@ -21,17 +21,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
@@ -39,6 +35,7 @@ import java.util.Map;
 
 @RestController
 @Log4j2
+@CrossOrigin(origins = { "*" })
 public class VisitorRegistrationController {
 
 	@Autowired
